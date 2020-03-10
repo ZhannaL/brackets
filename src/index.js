b34bb16b833b9aa74a9cde35d095c5117c481d68
ZhannaL <zhannet1814@gmail.com>
2020-03-10 16:38:49 +0100

    solution of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arrStr = str.split('');
+
+  const openBrackets = new Set(bracketsConfig.map(elConfig => elConfig[0]));
+  const closeByOpenBrackets = new Map(bracketsConfig.map(elConfig => [elConfig[1],elConfig[0]]));
+  const stack = [];
+
+  for (const el of arrStr){
+    if (openBrackets.has(el)) {
+      if (el === stack[stack.length-1] 
+        && openBrackets.has(el) 
+        && closeByOpenBrackets.get(el)) {
+        stack.pop()
+      } else { 
+        stack.push(el)
+      }
+    } else {
+      let lastSymbol = stack.pop();
+      if (lastSymbol !== closeByOpenBrackets.get(el)) return false
+    }
+  }
+ 
+  return stack.length===0
 }
